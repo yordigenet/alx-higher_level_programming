d7f4e3e608314206ccc6a475aaf00b3c37130115
Yordanos_Girma <yordigenet@gmail.com>
2022-09-13 10:11:08 -0700

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Represents declaretion of a class"""
-  class Square:
+
+
+class Square:
     """Represents a square.
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
