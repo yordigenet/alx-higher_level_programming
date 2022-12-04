cda30dd437c61a5b99c0ff03545078211403b391
Yordanos_Girma <yordigenet@gmail.com>
2022-12-04 10:51:48 -0800

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Lists all states starting with N"""
+"""Lists all states"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="hbtn_0e_0_usa",
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""
-SELECT * FROM states ORDER BY states.id ASC
-""")
+    cur.execute("SELECT * FROM states states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1].startswith("N"):
-            print(row)
+        print(row)
     cur.close()
     conn.close()
