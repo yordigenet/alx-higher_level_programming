bb016194dd9b4dc7a4f5b29070a39eb903f1badb
yordigenet <yordigenet@example.com>
2023-01-12 01:12:02 -0800

    new

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Curls request and display bytes received
-curl -so /dev/null -w '%{size_download}\n' "$1"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
