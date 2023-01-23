c5f014621986434d09e92f23b9b731cd435a1753
yordigenet <yordigenet@example.com>
2023-01-23 03:48:46 -0800

    updates

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Lists all states"""
-
-import MySQLdb
+""" script that lists all states from the database hbtn_0e_0_usa """
 from sys import argv
-
+import MySQLdb
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+    try:
+        stmt = "SELECT * FROM states ORDER BY id ASC"
+        cur.execute(stmt)
+        rtn = cur.fetchall()
+    except MySQLdb.Error:
+        try:
+            rtn = ("MySQLdb Error")
+        except IndexError:
+            rtn = ("MySQLdb Error - IndexError")
+    for i in rtn:
+        print(i)
     cur.close()
     conn.close()
